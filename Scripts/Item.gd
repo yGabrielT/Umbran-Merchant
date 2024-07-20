8d015d1b8759a453fee51bc82b6546efddfa07a3
Gabriel Trindade Dias <95586355+yGabrielT@users.noreply.github.com>
2024-07-20 20:38:13 -0300

    last adjustments

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -3,6 +3,8 @@ extends StaticBody3D
 @export var itemName : String
 @export var itemDesc : String
 
+@export var liquids = ["50red", "20red", "20blue"]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
