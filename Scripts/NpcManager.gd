d87ab7a8c1787281a62903e95f5c1a566fddbc7d
Gabriel Trindade Dias <95586355+yGabrielT@users.noreply.github.com>
2024-07-27 19:05:56 -0300

    a bit more of npcs

--- Scripts/NpcManager.gd
+++ Scripts/NpcManager.gd
@@ -38,31 +38,34 @@ func _process(delta):
 					for i in len(PotsInDeliverArea):
 						PotsInDeliverArea.shuffle() 
 						#What is the number of liquids needed in the pot?
-						if len(allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected) == len(PotsInDeliverArea[i - 1].liquids): 
-							
-							
-							if(potLiquidChecker != 0): #continue if not all checks are done
-								potLiquidChecker = len(allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected) # gives the value that needs to be subtratec
-								for x in len(PotsInDeliverArea[i - 1].liquids): #look at the liquids at the individual pot
-									PotsInDeliverArea[i - 1].liquids.shuffle() 
-									#Transform both Vector4 To Color8 and round it
-									var npcColorVec4 = Vector4(
-										allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.r,
-										allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.g,
-										allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.b,
-										allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.a) 
-									var npcColor = round(npcColorVec4 * 255) 
-									var potColor = round(PotsInDeliverArea[i - 1].liquids[x - 1] * 255)
-									#Check if the colors are equal
-									if  potColor == npcColor and not allNpcs[currentNpcNumber].PotionList[i - 1].isChecked and not allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].isChecked:
-										allNpcs[currentNpcNumber].PotionList[i - 1].isChecked = true
-										allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].isChecked
-										potLiquidChecker -=  1#Subtract liquid value
-										
-									
-							else:
+						if len(allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected) == 0:
+							potChecker = 0
+						else:
+							if len(allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected) == len(PotsInDeliverArea[i - 1].liquids): 
 								
-								potChecker -= 1 #Subtract pot value
+								
+								if(potLiquidChecker != 0): #continue if not all checks are done
+									potLiquidChecker = len(allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected) # gives the value that needs to be subtratec
+									for x in len(PotsInDeliverArea[i - 1].liquids): #look at the liquids at the individual pot
+										PotsInDeliverArea[i - 1].liquids.shuffle() 
+										#Transform both Vector4 To Color8 and round it
+										var npcColorVec4 = Vector4(
+											allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.r,
+											allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.g,
+											allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.b,
+											allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].liquidColor.a) 
+										var npcColor = round(npcColorVec4 * 255) 
+										var potColor = round(PotsInDeliverArea[i - 1].liquids[x - 1] * 255)
+										#Check if the colors are equal
+										if  potColor.is_equal_approx(npcColor) and not allNpcs[currentNpcNumber].PotionList[i - 1].isChecked and not allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].isChecked:
+											allNpcs[currentNpcNumber].PotionList[i - 1].isChecked = true
+											allNpcs[currentNpcNumber].PotionList[i - 1].liquidsQuantityExpected[x - 1].isChecked
+											potLiquidChecker -=  1#Subtract liquid value
+											
+										
+								else:
+									potChecker -= 1 #Subtract pot value
+							
 		elif not isNpcDone:
 			#All check are done
 			hasTalked = false
@@ -97,6 +100,7 @@ func _process(delta):
 			
 	if NpcScene != null:
 		if NpcScene.isGone :
+			
 			currentNpcNumber += 1
 			NpcScene.queue_free()
 			SpawnNpc()
--- Scripts/npcCheckList.gd
+++ Scripts/npcCheckList.gd
@@ -1,8 +1,9 @@
 extends Resource
 
 class_name check_list
-
+@export var anyBodyNeeded : bool
 @export var PotionList : Array[potions]
 @export var dialogueEnter : dialogue
 @export var dialogueExit : dialogue
 @export var idleAnim123 : int = 1
+
